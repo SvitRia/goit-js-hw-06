a36322817de1d415096b901a4f5cfec60fe60ff5
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-07-28 10:41:47 +0200

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,8 @@
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +17,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imageList = document.querySelector(".gallery")
+const list = images.map((image) => `<li><img scr="${image.url}"alt="${image.alt}"></li>`).join("")
+console.dir(list);
+
+imageList.insertAdjacentHTML("afterbegin",list)
+console.dir(imageList);
